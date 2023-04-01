6fe700f44a468098474fa94cd6ab740a4f5b56a6
Amit Melamed <amitpay1234@gmail.com>
2023-04-01 14:15:19 +0300

    Shooting Timer
    Timer for Shooting laser added

--- Assets/Scripts/2-spawners/LaserShooter.cs
+++ Assets/Scripts/2-spawners/LaserShooter.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 /**
  * This component spawns the given laser-prefab whenever the player clicks a given key.
@@ -6,8 +7,31 @@
  */
 public class LaserShooter: ClickSpawner {
     [SerializeField] NumberField scoreField;
+    [SerializeField] float timeBetweenShots = 0.5f;
+    private bool canShoot = true;
 
+
+
+    IEnumerator activateShooterTimer()
+    {
+        yield return new WaitForSeconds(timeBetweenShots);// Wait for one second
+
+        canShoot = true;
+        // All your Post-Delay Logic goes here:
+        // Run functions
+        // Set your Values
+        // Or whatever else
+    }
     protected override GameObject spawnObject() {
+        if (!canShoot) 
+        {
+            return null;
+        }
+        //After Shooting, set canShoot to false and start the timer.
+        //When the timer is done canShoot will be true and we will able to use spawnObject again.
+        canShoot=false;
+        StartCoroutine(activateShooterTimer());
+
         GameObject newObject = base.spawnObject();  // base = super
 
         // Modify the text field of the new object.
